df4f67d204de804f41ff34f8eaa904074bb5083c
Simon Scarfe <simon.scarfe@slalom.com>
2024-08-29 21:55:43 +0100

    save github PAT in options page

--- src/options.ts
+++ src/options.ts
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("api-key-form") as HTMLFormElement;
   const apiKeyInput = document.getElementById("api-key") as HTMLInputElement;
+  const patInput = document.getElementById("pat") as HTMLInputElement;
   const status = document.getElementById("status") as HTMLParagraphElement;
 
-  // Load and display the saved API key when the options page is loaded
-  chrome.storage.sync.get("apiKey", (data) => {
-    if (data.apiKey) {
-      apiKeyInput.value = data.apiKey;
-    }
+  chrome.storage.sync.get(["apiKey", "pat"], (data) => {
+    apiKeyInput.value = data.apiKey ?? "";
+    patInput.value = data.pat ?? "";
   });
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const apiKey = apiKeyInput.value;
-    chrome.storage.sync.set({ apiKey }, () => {
-      status.textContent = "API Key saved!";
+    const pat = patInput.value;
+    chrome.storage.sync.set({ apiKey, pat }, () => {
+      status.textContent = "Options saved!";
       setTimeout(() => {
         status.textContent = "";
       }, 2000);
